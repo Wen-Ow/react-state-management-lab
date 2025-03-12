f4973b230689f1c799fb71dd6c6fe75202d75398
Wen Ow <ow.wen2022@gmail.com>
2025-03-12 17:44:44 -0400

    saving progress on 1-3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,95 @@
-import './App.css'
+import { useState } from "react"; // Import React and useState Hook
+import "./App.css"; // Import CSS file
 
+// This is the main React component that will handle all the logic and UI
 function App() {
- 
+  // Define state variables using useState Hook
+  const [team, setTeam] = useState([]); // Stores the list of characters currently selected by the user.
+  const [money, setMoney] = useState(100); // Stores the amount of money the user has. Starts at $100 and updates when adding/removing team members.
+  const [zombieFighters, setZombieFighters] = useState([ // Stores the list of available zombie fighters or characters.
+    {
+      id: 1,
+      name: "Survivor",
+      price: 12,
+      strength: 6,
+      agility: 4,
+      img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/0c2d6b.png",
+    },
+    {
+      id: 2,
+      name: "Scavenger",
+      price: 10,
+      strength: 5,
+      agility: 5,
+      img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/033a16.png",
+    },
+    {
+      id: 3,
+      name: "Shadow",
+      price: 18,
+      strength: 7,
+      agility: 8,
+      img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/262c36.png",
+    },
+    {
+      id: 4,
+      name: "Tracker",
+      price: 14,
+      strength: 7,
+      agility: 6,
+      img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/3c1e70.png",
+    },
+    {
+      id: 5,
+      name: "Sharpshooter",
+      price: 20,
+      strength: 6,
+      agility: 8,
+      img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/4b2900.png",
+    },
+    {
+      id: 6,
+      name: "Medic",
+      price: 15,
+      strength: 5,
+      agility: 7,
+      img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/5a1e02.png",
+    },
+    {
+      id: 7,
+      name: "Engineer",
+      price: 16,
+      strength: 6,
+      agility: 5,
+      img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/5e103e.png",
+    },
+    {
+      id: 8,
+      name: "Brawler",
+      price: 11,
+      strength: 8,
+      agility: 3,
+      img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/67060c.png",
+    },
+    {
+      id: 9,
+      name: "Infiltrator",
+      price: 17,
+      strength: 5,
+      agility: 9,
+      img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/ac3220.png",
+    },
+    {
+      id: 10,
+      name: "Leader",
+      price: 22,
+      strength: 7,
+      agility: 6,
+      img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/e41f26.png",
+    },
+  ]);
 
-  return (
-    <>
-      
-    </>
-  )
+  return <></>;
 }
 
-export default App
+export default App;
